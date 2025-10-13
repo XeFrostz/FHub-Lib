7e2b644c57c3cb397f92c944dee44a98637d9991
XeFrostz <116756996+XeFrostz@users.noreply.github.com>
2025-10-14 00:31:21 +0700

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -205,7 +205,7 @@ do
 		end
 	end
 
-	local IconList = loadstring(game:HttpGet('https://raw.githubusercontent.com/Dummyrme/Library/refs/heads/main/Icon.lua'))()
+	local IconList = loadstring(game:HttpGet('https://raw.githubusercontent.com/XeFrostz/FHub-Lib/refs/heads/main/icon.lua'))()
 	function gl(i)
 		local iconData = IconList.Icons[i]
 		if iconData then
